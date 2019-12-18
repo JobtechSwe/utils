da49a816c653d4d9616d53504b6bbd481a259092
Lina Bao <linabao@Linas-MacBook-Pro.local>
2019-12-18 11:13:08 +0100

    Add mask sensitive info tool

---
+++ utils.py
@@ -0,0 +1,11 @@
+import re
+
+
+def mask_sensitive_info(ads: str) -> str:
+    email_regex = '\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})'
+    personal_no_regex = '\d{6,8}[-|(\s)]{0,1}[T|\d]\d{3}'
+    telephone_regex = r'(?:\B\+ ?49|\b0)(?: *[(-]? *\d(?:[ \d]*\d)?)? *(?:[)-] *)?\d+ *(?:[/)-] *)?\d+ *(?:[/)-] *)?\d+(?: *- *\d+)?'
+    result = re.sub(email_regex, 'EMAIL', ads)
+    result = re.sub(personal_no_regex, 'PERSONAL NO', result)
+    result = re.sub(telephone_regex, 'TELEPHONE NO', result)
+    return result
