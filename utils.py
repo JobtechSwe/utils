ade0396ad0204afdef49abaab00dc460feee1024
Lina Bao <linabao@Linas-MacBook-Pro.local>
2020-01-02 15:23:10 +0100

    Add funtion to build dictionary

--- utils.py
+++ utils.py
@@ -1,4 +1,63 @@
 import re
+import collections
+
+
+def build_dictionary_lastname():
+    # build Trie Dictionary for last name
+    dictionary_lastname = collections.defaultdict()
+    with open('last_name.txt', 'r') as file:
+        for line in file.readlines():
+            dic = dictionary_lastname
+            for w in line:
+                dic = dic.setdefault(w, {})
+            dic.setdefault('_end')
+    return dictionary_lastname
+
+
+def build_dictionary_firstname():
+    # build Trie Dictionary for first name
+    dictionary_firstname = collections.defaultdict()
+    with open('first_name.txt', 'r') as file:
+        for line in file.readlines():
+            dic = dictionary_firstname
+            for w in line:
+                dic = dic.setdefault(w, {})
+            dic.setdefault('_end')
+    return dictionary_firstname
+
+
+def check_name(word: str, dictionary):
+    name = dictionary
+    for letter in word:
+        if letter not in name:
+            return False
+        name = name[letter]
+    if "_end" in name:
+        return True
+    return False
+
+
+def find_name(ads: str):
+    ads = ads.split(' ')
+    length = len(ads)
+    dictionary_firstname = build_dictionary_firstname()
+    dictionary_lastname = build_dictionary_lastname()
+    i = 0
+    while i < length:
+        print(check_name(ads[i], dictionary_firstname))
+        if check_name(ads[i], dictionary_firstname):
+            next = i + 1
+            while next < length:
+                if check_name(ads[next], dictionary_lastname):
+                    next += 1
+                else:
+                    break
+            if next - i > 1:
+                ads[i:next] = '**NAME***'
+                i = next - 1
+        i += 1
+        return ads
+
 
 def mask_sensitive_info(ads: str) -> str:
     email_regex = '\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})'
@@ -8,3 +67,9 @@ def mask_sensitive_info(ads: str) -> str:
     result = re.sub(personal_no_regex, 'PERSONAL NO', result)
     result = re.sub(telephone_regex, 'TELEPHONE NO', result)
     return result
+
+
+
+
+
+
