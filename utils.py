ae508f5508106927d942332567ff4f8595f18333
Lina Bao <linabao@Linas-MacBook-Pro.local>
2020-01-07 10:22:03 +0100

    Update the object name

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import re
 import collections
 
 
-class MaskSensitiveInfo:
+class Advertisements:
     def __init__(self, ads):
         self.ads = ads
 
@@ -62,8 +62,8 @@ class MaskSensitiveInfo:
         email_regex = '\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})'
         personal_no_regex = '\d{6,8}[-|(\s)]{0,1}[T|\d]\d{3}'
         telephone_regex = r'(?:\B\+ ?49|\b0)(?: *[(-]? *\d(?:[ \d]*\d)?)? *(?:[)-] *)?\d+ *(?:[/)-] *)?\d+ *(?:[/)-] *)?\d+(?: *- *\d+)?'
-        result = re.sub(email_regex, 'EMAIL', ads)
-        result = re.sub(personal_no_regex, 'PERSONAL NO', result)
-        result = re.sub(telephone_regex, 'TELEPHONE NO', result)
+        result = re.sub(email_regex, '**EMAIL***', ads)
+        result = re.sub(personal_no_regex, '**PERSONAL NO**', result)
+        result = re.sub(telephone_regex, '**TELEPHONE NO**', result)
         result = self.find_name(result)
         return result
