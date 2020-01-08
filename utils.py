52c7173f359cd2a15695616567baf53386e1973d
lina bao <lina7906@gmail.com>
2020-01-08 09:51:58 +0100

    Update telephone regex

--- utils.py
+++ utils.py
@@ -29,6 +29,8 @@ class Advertisements:
         return dictionary_firstname
 
     def check_name(self, word: str, dictionary):
+        #Check if it is first name in first name dictionary, or if it is last name in last name dictionary.
+        #If it is return true if it is not return False
         name = dictionary
         for letter in word:
             if letter not in name:
@@ -39,6 +41,7 @@ class Advertisements:
         return False
 
     def find_name(self, ads: str):
+        # Find full name and mask name
         ads = ads.split(' ')
         length = len(ads)
         dictionary_firstname = self.build_dictionary_firstname()
@@ -53,17 +56,17 @@ class Advertisements:
                     else:
                         break
                 if next - i > 1:
-                    ads[i:next] = '**NAME***'
+                    ads[i:next] = '***NAME***'
                     i = next - 1
             i += 1
         return ' '.join(ads)
 
-    def mask_sensitive_info(self, ads: str) -> str:
+    def mask_sensitive_info(self) -> str:
         email_regex = '\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})'
-        personal_no_regex = '\d{6,8}[-|(\s)]{0,1}[T|\d]\d{3}'
-        telephone_regex = r'(?:\B\+ ?49|\b0)(?: *[(-]? *\d(?:[ \d]*\d)?)? *(?:[)-] *)?\d+ *(?:[/)-] *)?\d+ *(?:[/)-] *)?\d+(?: *- *\d+)?'
-        result = re.sub(email_regex, '**EMAIL***', ads)
-        result = re.sub(personal_no_regex, '**PERSONAL NO**', result)
-        result = re.sub(telephone_regex, '**TELEPHONE NO**', result)
+        personal_no_regex = '[19|20]{0,2}[0-9][0-9](0[1-9]|1[0-2])(0[1-9]|1[1-9]|2[1-9]|3[0|1])[-|(\s)]{0,1}[T|\d]\d{3}'
+        telephone_regex = r'[(]{0,1}[00|\+]{0,2}[0-9]{1,4}[)]{0,1}\s*[0]{0,1}[1-9]\s*[-]{0,1}\s*[0-9]\s*[-]{0,1}\s*[0-9]\s*[-]{0,1}\s*[0-9]\s*[0-9]\s*[0-9]\s*[0-9]\s*[0-9]{0,1}\s*[0-9]{0,1}\s*'
+        result = re.sub(email_regex, '* * * EMAIL * * *', self.ads)
+        result = re.sub(personal_no_regex, '* * * PERSONAL NO * * *', result)
+        result = re.sub(telephone_regex, '* * * TELEPHONE NO * * *', result)
         result = self.find_name(result)
         return result
